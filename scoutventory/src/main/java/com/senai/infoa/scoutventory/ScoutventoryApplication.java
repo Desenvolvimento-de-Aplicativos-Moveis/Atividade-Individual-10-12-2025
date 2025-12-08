10bb8f2f49876b6e970ba92301978ee42b5c862a
Nicolas Esteves <nicolas.e.moreira@gmail.com>
2025-12-08 11:09:09 -0300

    Commit Inicial

---
+++ scoutventory/src/main/java/com/senai/infoa/scoutventory/ScoutventoryApplication.java
@@ -0,0 +1,13 @@
+package com.senai.infoa.scoutventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoutventoryApplication.class, args);
+	}
+
+}
---
+++ scoutventory/src/test/java/com/senai/infoa/scoutventory/ScoutventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.infoa.scoutventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoutventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
